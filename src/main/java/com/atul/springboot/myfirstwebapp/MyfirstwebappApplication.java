8aad43117cdb69ea1bce04fca8eb0b5651eacd17
atuldwivedi01 <dwivedibittuatul@gmail.com>
2023-07-08 11:36:30 +0530

    init push

---
+++ src/main/java/com/atul/springboot/myfirstwebapp/MyfirstwebappApplication.java
@@ -0,0 +1,13 @@
+package com.atul.springboot.myfirstwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstwebappApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into todo(ID,USERNAME,DESCRIPTION,TARGET_DATE,DONE)
+values(10001,'atul','Get AWS Certified',CURRENT_DATE(),false);
+insert into todo(ID,USERNAME,DESCRIPTION,TARGET_DATE,DONE)
+values(10002,'atul','Get GCP Certified',CURRENT_DATE(),false);
+insert into todo(ID,USERNAME,DESCRIPTION,TARGET_DATE,DONE)
+values(10003,'atul','Learn DevOps',CURRENT_DATE(),false);
